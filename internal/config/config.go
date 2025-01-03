b5d909f196c16caf08e4052bb6e3e5f4d65580a8
LeAries <learies@ya.ru>
2025-01-03 12:07:22 +0300

    Add flag parse

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,37 @@
 package config
 
+import (
+	"flag"
+	"os"
+)
+
 type Config struct {
 	Address string
 	BaseURL string
 }
 
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
 func NewConfig() (*Config, error) {
-	defaultAddress := "localhost:8080"
-	defaultBaseURL := "http://localhost:8080"
+	defaultAddress := ":8080"
+	defaultBaseURL := "http://localhost" + defaultAddress
+
+	envAddress := getEnv("SERVER_ADDRESS", defaultAddress)
+	envBaseURL := getEnv("BASE_URL", defaultBaseURL)
+
+	address := flag.String("a", envAddress, "address to start the HTTP server")
+	baseURL := flag.String("b", envBaseURL, "base URL for shortened URLs")
+
+	flag.Parse()
 
 	return &Config{
-		Address: defaultAddress,
-		BaseURL: defaultBaseURL,
+		Address: *address,
+		BaseURL: *baseURL,
 	}, nil
 }
