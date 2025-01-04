30a1efeab01494202f3d245818c4bfb443920709
LeAries <learies@ya.ru>
2025-01-04 15:09:56 +0300

    Add filePath

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Config struct {
-	Address string
-	BaseURL string
+	Address  string
+	BaseURL  string
+	FilePath string
 }
 
 func getEnv(key, defaultValue string) string {
@@ -21,17 +22,21 @@ func getEnv(key, defaultValue string) string {
 func NewConfig() (*Config, error) {
 	defaultAddress := ":8080"
 	defaultBaseURL := "http://localhost" + defaultAddress
+	var defaultFilePath string
 
 	envAddress := getEnv("SERVER_ADDRESS", defaultAddress)
 	envBaseURL := getEnv("BASE_URL", defaultBaseURL)
+	envFilePath := getEnv("FILE_STORAGE_PATH", defaultFilePath)
 
 	address := flag.String("a", envAddress, "address to start the HTTP server")
 	baseURL := flag.String("b", envBaseURL, "base URL for shortened URLs")
+	filePath := flag.String("f", envFilePath, "path to the file for storing URL data")
 
 	flag.Parse()
 
 	return &Config{
-		Address: *address,
-		BaseURL: *baseURL,
+		Address:  *address,
+		BaseURL:  *baseURL,
+		FilePath: *filePath,
 	}, nil
 }
