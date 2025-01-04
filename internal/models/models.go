fc7959e4da1e9a9dbce46c0d28578b08c03d87cb
LeAries <learies@ya.ru>
2025-01-04 15:11:19 +0300

    Add ShortenStore

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,19 @@
 package models
 
+import (
+	"github.com/gofrs/uuid"
+)
+
 type ShortenRequest struct {
 	URL string `json:"url"`
 }
 
 type ShortenResponse struct {
 	Result string `json:"result"`
+}
+
+type ShortenStore struct {
+	UUID        uuid.UUID `json:"uuid"`
+	ShortURL    string    `json:"short_url"`
+	OriginalURL string    `json:"original_url"`
 }
