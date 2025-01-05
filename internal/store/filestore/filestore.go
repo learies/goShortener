5251213e3655020cb9208cb701fa7187876329eb
LeAries <learies@ya.ru>
2025-01-05 09:33:45 +0300

    Add SaveToFile and LoadFromFile

--- internal/store/filestore/filestore.go
+++ internal/store/filestore/filestore.go
@@ -1,10 +1,15 @@
 package filestore
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 	"sync"
 
+	"github.com/google/uuid"
+
 	"github.com/learies/goShortener/internal/config/logger"
+	"github.com/learies/goShortener/internal/models"
 )
 
 var ErrURLNotFound = errors.New("URL not found")
@@ -12,6 +17,7 @@ var ErrURLNotFound = errors.New("URL not found")
 type FileStore struct {
 	URLMapping map[string]string
 	mu         sync.Mutex
+	FilePath   string
 }
 
 func (fs *FileStore) Add(shortURL, originalURL string) error {
@@ -20,6 +26,7 @@ func (fs *FileStore) Add(shortURL, originalURL string) error {
 
 	fs.URLMapping[shortURL] = originalURL
 
+	fs.SaveToFile()
 	logger.Log.Info("Added to store", "shortURL", shortURL, "originalURL", originalURL)
 
 	return nil
@@ -29,6 +36,12 @@ func (fs *FileStore) Get(shortURL string) (string, error) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
+	err := fs.LoadFromFile(fs.FilePath)
+	if err != nil {
+		logger.Log.Error("Failed to load from file", "error", err)
+		return "", err
+	}
+
 	originalURL, ok := fs.URLMapping[shortURL]
 	if !ok {
 		return "", ErrURLNotFound
@@ -37,4 +50,57 @@ func (fs *FileStore) Get(shortURL string) (string, error) {
 	logger.Log.Info("Retrieved from store", "shortURL", shortURL, "originalURL", originalURL)
 
 	return originalURL, nil
+}
+
+func (fs *FileStore) SaveToFile() error {
+	file, err := os.Create("store.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+
+	for shortURL, originalURL := range fs.URLMapping {
+		record := models.ShortenStore{
+			UUID:        uuid.New(),
+			ShortURL:    shortURL,
+			OriginalURL: originalURL,
+		}
+
+		// Записываем объект ShortenStore в файл.
+		if err := encoder.Encode(record); err != nil {
+			return err
+		}
+
+		logger.Log.Info("Saving to file", "uuid", record.UUID, "short_url", shortURL, "original_url", originalURL)
+	}
+
+	return nil
+}
+
+func (fs *FileStore) LoadFromFile(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	for {
+		var record models.ShortenStore
+		if err := decoder.Decode(&record); err != nil {
+			break
+		}
+
+		fs.URLMapping[record.ShortURL] = record.OriginalURL
+	}
+
+	logger.Log.Info("Loaded from file", "URLMapping", fs.URLMapping)
+
+	return nil
 }
