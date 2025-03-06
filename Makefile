c3148c1711eafa1f8ed1ddd9594479e6f22fbba7
learies <learies@ya.ru>
2025-03-06 20:13:22 +0300

    Add Makefile

--- Makefile
+++ Makefile
@@ -2,10 +2,15 @@ BINARY_NAME=shortener
 
 BUILD_DIR=cmd/shortener
 
+# Get git commit hash
+COMMIT_HASH=$(shell git rev-parse --short HEAD)
+BUILD_DATE=$(shell date -u +"%Y-%m-%d_%H:%M:%S")
+VERSION=1.0.0
+
 all: build
 
 build:
-	cd $(BUILD_DIR) && go build -o $(BINARY_NAME)
+	cd $(BUILD_DIR) && go build -ldflags "-X main.buildVersion=$(VERSION) -X main.buildDate=$(BUILD_DATE) -X main.buildCommit=$(COMMIT_HASH)" -o $(BINARY_NAME)
 
 run:
 	go run $(BUILD_DIR)/main.go
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/learies/goShortener/internal/app"
 	"github.com/learies/goShortener/internal/config"
 	"github.com/learies/goShortener/internal/config/logger"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	fmt.Printf("Build version: %s\n", getValueOrDefault(buildVersion))
+	fmt.Printf("Build date: %s\n", getValueOrDefault(buildDate))
+	fmt.Printf("Build commit: %s\n", getValueOrDefault(buildCommit))
+}
+
+func getValueOrDefault(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
+}
+
 // main is the entry point for the application.
 func main() {
+	printBuildInfo()
+
 	err := logger.NewLogger("info")
 	if err != nil {
 		logger.Log.Error("Error creating logger", "error", err)
